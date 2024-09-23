588381bf814bb521bb8d7c3eda9968db6158f8fc
Bruno Biletsky <brunobiletsky@gmail.com>
2024-09-23 11:52:29 -0300

    iniciando login e dados.json

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,29 @@
 import { useNavigate } from "react-router-dom"
-
+import { useRef, useState} from "react"
 
 const Login=()=>{
+    // usando useRef para identificar os elementos
+    const usuario = useRef();
+    const senha = useRef();
+
+    // usando useState para alterar a variavel (criando array para diversos usuarios)
+    const [usuarios, setUsuarios]= useState([]);
+
+    // criando useNavigate para redirecionar para CadastrarProdutos
+    const navigate = useNavigate();
+
+    // validando o login
+    function validade (){
+        for(let i=0; i<usuarios.length; i++){
+            if(
+                usuarios[i].usuario== usuario.current.value &&
+                usuarios[i].usuario== usuario.current.value
+            ){
+                return true;
+            }
+        }
+    }
+    
     return(
         <>
         
