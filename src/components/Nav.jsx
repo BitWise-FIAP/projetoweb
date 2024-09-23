0e7ca686ad9caca7483053fdf3d5b2ed84ab3ebd
Bruno Biletsky <brunobiletsky@gmail.com>
2024-09-23 20:24:24 -0300

    primeira estilizacao nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom"
-
+import { NavStyle } from "../css/NavStyle"
+import fundo from "../assets/fundo-nav.png"
 const Nav=()=>{
     return(
         <>
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/produtos">Produtos</Link>
-            <Link to="/sobre">Sobre</Link>
-        </nav>
-        <hr/>
+        <NavStyle>
+            <div className="nav-container">
+                <img className="img1" src={fundo}></img>
+                
+                <nav>
+                    <ul>
+                        <li><Link to="/"><p>Home</p></Link></li>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/produtos">Produtos</Link></li>
+                        <li><Link to="/sobre">Sobre</Link></li>
+                    </ul>
+                    
+                    
+                    
+                    
+                </nav>
+            </div>
+        </NavStyle>
+        
         </>
     )
 }
--- src/css/NavStyle.jsx
+++ src/css/NavStyle.jsx
@@ -2,5 +2,51 @@ import styled from "styled-components";
 
 export const NavStyle = styled.section`
     
+:root{
+    --color1:#fff;
+    --color2:#000;
+    --color3:#134563;
+    --color4:#817278;
+    --color5:#4f698c;
+    --color6:#5c748e;
+    --color7:#8a9fb4;
+    --color7:#ccc;
+    
+}
+.img1{
+    object-fit: none;
+    position: absolute;
+    height: 9vh;
+    width: 100%;
+    filter: brightness(80%)
+}
+
+.nav-container{
+    width: 100%;
+    height: 9vh;
+    background-color: var(--color7);
+    position: relative;
+}
+nav ul{
+    display: flex;
+    margin-left: 65%;
+    justify-content: space-around;
+    padding-top: 2%;
+    list-style: none;
+}
+nav a{
+    text-decoration: none;
+    position: absolute;
+    color: var(--color1);
+    font-size: 30px;
+    transition: all .2s ease-in-out;
+}
+nav a:hover{
+    transition: .2s all ease-in-out;
+    transform: translateY(-4px);
+    color: var(--color7);
+}
+    
+
 
 `
