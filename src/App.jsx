7f46018d57fa44785567b3819eedfe8715f91928
Bruno Biletsky <brunobiletsky@gmail.com>
2024-09-23 09:29:11 -0300

    arrumando app com imports desnecessários e arrumando app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Nav from './components/Nav'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <>
-      
+      <Nav/>
+      <Outlet/>
+      <Footer/>
     </>
   )
 }
