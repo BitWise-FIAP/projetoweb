c4e6c1b3a5f2e40b633b9e5c463cc8f30e761ce9
Enrico <errodrigues2006@outlook.com.br>
2024-09-23 13:10:17 -0300

    Adicionando link em produtos

--- src/routes/CadastrarProduto.jsx
+++ src/routes/CadastrarProduto.jsx
@@ -1,9 +1,18 @@
 
+import { CadastrarProdutoStyle } from "../css/CadastrarProdutoStyle"
+import { useState,useRef,useEffect } from "react";
+
 
 const CadastrarProduto=()=>{
+    const usuario = useRef()
     return(
         <>
-        
+            <CadastrarProdutoStyle>
+                <h1>Lista de produtos</h1>
+                <p>
+
+                </p>
+            </CadastrarProdutoStyle>
         </>
     )
 }
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom"
+import { useRef } from "react";
 
 const Produtos=()=>{
     return(
         <>
-        
+            <h1>lista de produtos</h1>
+            <Link to={sessionStorage.getItem("usuario") ? "/cadastrarProduto" : "/login"}>Cadastrar</Link>
         </>
     )
 }
