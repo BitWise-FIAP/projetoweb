9f6323f94d4912e9803ea049f21be62a25104ea5
Bruno Biletsky <brunobiletsky@gmail.com>
2024-09-23 09:22:29 -0300

    criando rotas e adicionando elemento erro

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './routes/Login.jsx'
+import Produtos from './routes/Produtos.jsx'
+import Home from './routes/Home.jsx'
+import Sobre from './routes/Sobre.jsx'
+import CadastrarProduto from './routes/CadastrarProduto.jsx'
 
+const router = createBrowserRouter([{
+  path:"/", element:<App/>,
+  errorElement: <Error/>,
 
+  children:[
+    {path:"/", element:<Home/>},
+    {path:"/login", element:<Login/>},
+    {path:"/produtos", element:<Produtos/>},
+    {path:"/sobre", element:<Sobre/>},
+    {path:"/cadastrarProduto",element:<CadastrarProduto/>}
+  ]
+}])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
---
+++ src/routes/Erro.jsx
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+const Erro=()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Erro
