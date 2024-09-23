a77945a783a12e251f3fd47d6f19255bdf813972
Bruno Biletsky <brunobiletsky@gmail.com>
2024-09-23 09:49:56 -0300

    icones criados no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
+import { SlSocialInstagram } from "react-icons/sl";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { FaFacebookSquare } from "react-icons/fa";
+import { TfiEmail } from "react-icons/tfi";
 
 
 
@@ -5,6 +9,13 @@ const Footer=()=>{
     return(
         <>
         <h5>@2024 - Todos direitos reservados</h5>
+        <a href="#fbook"><FaFacebookSquare/></a>
+        <a href="#instagram"><SlSocialInstagram/></a>
+        <a href="#linkedin"><SlSocialLinkedin/></a>
+        <a href="#email"><TfiEmail/></a>
+        
+
+
         </>
     )
 }
