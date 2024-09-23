f90fa2281bacf4af644d8578e723b65b52bd8243
Bruno Biletsky <brunobiletsky@gmail.com>
2024-09-23 20:37:39 -0300

    primeira estilo footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,24 @@ import { SlSocialInstagram } from "react-icons/sl";
 import { SlSocialLinkedin } from "react-icons/sl";
 import { FaFacebookSquare } from "react-icons/fa";
 import { TfiEmail } from "react-icons/tfi";
+import { FooterStyle } from "../css/FooterStyle";
 
 
 
 const Footer=()=>{
     return(
         <>
-        <h5>@2024 - Todos direitos reservados</h5>
-        <a href="#fbook"><FaFacebookSquare/></a>
-        <a href="#instagram"><SlSocialInstagram/></a>
-        <a href="#linkedin"><SlSocialLinkedin/></a>
-        <a href="#email"><TfiEmail/></a>
-    
+        <FooterStyle>
+            <section className="footer-container">
+                <h5>@2024 - Todos direitos reservados</h5>
+                <div className="icons">
+                    <a href="#fbook"><FaFacebookSquare/></a>
+                    <a href="#instagram"><SlSocialInstagram/></a>
+                    <a href="#linkedin"><SlSocialLinkedin/></a>
+                    <a href="#email"><TfiEmail/></a>
+                </div>
+            </section>
+        </FooterStyle>
         </>
     )
 }
--- src/css/FooterStyle.jsx
+++ src/css/FooterStyle.jsx
@@ -1,6 +1,36 @@
 import styled from "styled-components";
 
 export const FooterStyle = styled.section`
+:root{
+    --color1:#fff;
+    --color2:#000;
+    --color3:#134563;
+    --color4:#817278;
+    --color5:#4f698c;
+    --color6:#5c748e;
+    --color7:#8a9fb4;
+    --color7:#ccc;
     
+}
+
+.footer-container{
+    display: grid;
+    justify-content: center;
+    padding: 2vh;
+    background-color: var(--color3);
+    height: 10vh;
+}
+.footer-container h5{
+    color: var(--color7);
+    font-size: 1.2rem
+}
+.icons{
+    display: flex;
+    justify-content: center;
+}
+.icons a{
+    padding: 0.5vh;
+    color: var(--color7);
+}
 
 `
